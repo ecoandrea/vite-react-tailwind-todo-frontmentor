8e6f8eb51141110eab0bcb366a68fb7614c8b2a8
Cristina Zamora <ecoandrea@hotmail.com>
2022-10-20 21:05:44 -0300

    dia 3 localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import TodoComputed from "./components/TodoComputed";
 import TodoCreate from "./components/TodoCreate";
 import TodoFilter from "./components/TodoFilter";
 import TodoList from "./components/TodoList";
-
+/*
 const initialStateTodos = [
     {
         id: 1,
@@ -16,9 +16,15 @@ const initialStateTodos = [
     { id: 4, title: "Pick up groceries", completed: true },
     { id: 5, title: "Complete todo app on Frontend Mentor", completed: false },
 ];
+*/
+const initialStateTodos = JSON.parse(localStorage.getItem("todos")) || [];
 
 const App = () => {
     const [todos, setTodos] = useState(initialStateTodos);
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
 
     const createTodo = (title) => {
         const newTodo = {
@@ -66,10 +72,10 @@ const App = () => {
     };
 
     return (
-        <div className="min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
+        <div className="md:bg-[url('./assets/images/bg-desktop-light.jpg') md:dark:bg-[url('./assets/images/bg-desktop-light.jpg')  min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] ">
             <Header />
 
-            <main className="container mx-auto mt-8 px-4">
+            <main className="container mx-auto mt-8 px-4 md:max-w-xl ">
                 <TodoCreate createTodo={createTodo} />
 
                 <TodoList
@@ -86,7 +92,7 @@ const App = () => {
                 <TodoFilter changeFilter={changeFilter} filter={filter} />
             </main>
 
-            <footer className="mt-8 text-center">
+            <footer className="datk:text-gray-400 mt-8 text-center">
                 Drag and drop to reorder list
             </footer>
         </div>
